8838c5f7142c794270a9bd7bb21391ae522d9131
vh2504 <dangvanhoa077@gmail.com>
2020-07-14 22:26:14 +0700

    done Genk

--- Genk/1.js
+++ Genk/1.js
@@ -1,14 +1,32 @@
 $(function(){
 	$(".menu-con").addClass("an");
 	$(".fa").hover(function() {
+		$(this).removeClass("fa-bars");
+		$(this).html("X");
+		$(this).addClass("chuX");
 		$(".menu-con").addClass("hien");
+
 	}, function() {
 		// $(".menu-con").removeClass("hien");
 		// $(".menu-con").addClass("an");
 	});
 	$(".menu-con").mouseleave(function() {
-		console.log("da roi");
+		$(".menu-con").removeClass("hien");
+		$(".menu-con").addClass("an");
+		$(".chuX").addClass("fa-bars");
+		$(".chuX").removeClass("chuX");
+		$(".fa").html("");
+			
+	});
+
+	$(".chuX").hover(function() {
 			$(".menu-con").removeClass("hien");
 			$(".menu-con").addClass("an");
+			$(".chuX").addClass("fa-bars");
+			$(".chuX").removeClass("chuX");
+			$(".fa").html("");
+	}, function() {
+		/* Stuff to do when the mouse leaves the element */
 	});
+
 });
